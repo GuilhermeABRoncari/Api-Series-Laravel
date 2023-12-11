928c529734c139e7c3fdbfe10cd117b2cac678e9
GuilhermeABRoncari <guilhermeabroncari@hotmail.com>
2023-12-11 10:41:50 -0300

    Cria appends para os links relacionados a hyper-media

--- app/Models/Series.php
+++ app/Models/Series.php
@@ -2,13 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Series extends Model
 {
     use HasFactory;
     protected $fillable = ['name', 'cover_path'];
+    protected $appends = ['links'];
 
     public function seasons()
     {
@@ -18,5 +20,25 @@ class Series extends Model
     public function episodes()
     {
         return $this->hasManyThrough(Episode::class, Season::class);
+    }
+
+    public function links(): Attribute
+    {
+        return new Attribute(
+                fn () => [
+                [
+                    'ref'=> 'self',
+                    'url'=> "/api/series/{$this->id}"
+                ],
+                [
+                    'ref'=> 'seasons',
+                    'url'=> "/api/series/{$this->id}/seasons"
+                ],
+                [
+                    'ref'=> 'episodes',
+                    'url'=> "/api/series/{$this->id}/episodes"
+                ]
+            ]   
+        );
     }
 }
